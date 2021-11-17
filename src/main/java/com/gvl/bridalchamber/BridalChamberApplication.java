cd4bd07196ec0f4b073eaf3ba8cf4e29691a7148
DKenChi <zhaoxye@enn.cn>
2021-11-17 15:51:40 +0800

    Initial commit

---
+++ src/main/java/com/gvl/bridalchamber/BridalChamberApplication.java
@@ -0,0 +1,13 @@
+package com.gvl.bridalchamber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridalChamberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BridalChamberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gvl/bridalchamber/BridalChamberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvl.bridalchamber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridalChamberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
